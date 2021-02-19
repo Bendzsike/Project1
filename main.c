3ab97851aa6c4d913d52db243eac3ed865efc1f1
Bendzsike <bodor.benjamin@outlook.com>
2021-02-19 11:39:56 +0200

    Initialising a and b, and calculating their sum.

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int sum(int, int);
+
 int main() {
-    printf("Hello, World!\n");
+    int a, b;
+    printf("a =");
+    scanf("%d", &a);
+    printf("b =");
+    scanf("%d", &b);
+    printf("%d + %d = %d\n", a, b, sum(a, b));
     return 0;
+}
+
+int sum(int a, int b){
+    return a + b;
 }
