823088a3ced568fb4e1169391108c18bac8981ab
Bendzsike <bodor.benjamin@outlook.com>
2021-02-19 10:50:02 +0200

    Init Commit

---
+++ cmake-build-debug/CMakeFiles/Labor_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labor_1.dir/main.c.obj"
+  "Labor_1.exe"
+  "Labor_1.exe.manifest"
+  "Labor_1.pdb"
+  "libLabor_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labor_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
